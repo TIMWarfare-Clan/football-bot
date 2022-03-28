12fddf50ded020607330ae7273cbb97acf365c5d
koraynilay <koray.fra@gmail.com>
2022-03-28 22:34:12 +0200

    Add ElenaSport API

--- palone_bot.js
+++ palone_bot.js
@@ -4,16 +4,35 @@ var p = '/'; //prefix
 
 const endec = require(process.env.cm);
 const {Client, RichEmbed, Intents} = require('discord.js');
+var axios = require("axios").default;
+
+
 
 const client = new Client({
 	intents: ["GUILDS", "GUILD_MESSAGES"]
 });
 
-client.once('ready', () => {
+client.once('ready', async () => {
 	data_boot = new Date();
 	console.log(`Logged in as ${client.user.id} at ${data_boot}.\n`+
-		    `prefix: ${p}\n`
+		    `prefix: ${p}`
 	);
+	endpoint = '/v2/seasons/4270/fixtures?idTeam1=862&idTeam2=865'
+//	endpoint = '/v2/countries/45/leagues'
+	var options = {
+	  method: 'GET',
+	  url: 'https://elenasport-io1.p.rapidapi.com'+endpoint,
+	  headers: {
+	    'X-RapidAPI-Host': 'elenasport-io1.p.rapidapi.com',
+	    'X-RapidAPI-Key': endec.decode(process.env.es_api_key)
+	  }
+	};
+
+	try{
+		resp = await axios.request(options)
+		console.log(resp.data);
+	}catch(e){console.log(e)}
+
 });
 
 client.on('messageCreate', () => {
@@ -23,6 +42,7 @@ client.on('messageCreate', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 	console.log(interaction);
+	//interaction.commandName === 'ping'
 	await interaction.reply(interaction.options.getString('value'));
 });
 
