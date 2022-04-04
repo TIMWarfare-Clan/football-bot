52a503f52e3ff8d79700f7aa391caf3e45b29731
koraynilay <koray.fra@gmail.com>
2022-04-04 23:27:20 +0200

    Add jimp to make VS image

--- football-bot.js
+++ football-bot.js
@@ -37,6 +37,12 @@ try{
 
 	resp = { "data": { "data": [ { "id": 216852, "idCountry": 45, "countryName": "Italy", "idLeague": 318, "leagueName": "Serie A", "idSeason": 4270, "seasonName": "Serie A - 2021/2022", "idHome": 865, "homeName": "Atalanta", "idAway": 862, "awayName": "Bologna", "idStage": 4184, "idVenue": null, "venueName": null, "date": "2021-08-28 16:30:00", "status": "finished", "round": 2, "attendance": null, "team_home_90min_goals": 0, "team_away_90min_goals": 0, "team_home_ET_goals": 0, "team_away_ET_goals": 0, "team_home_PEN_goals": 0, "team_away_PEN_goals": 0, "team_home_1stHalf_goals": 0, "team_away_1stHalf_goals": 0, "team_home_2ndHalf_goals": 0, "team_away_2ndHalf_goals": 0, "elapsed": 0, "elapsedPlus": 0, "eventsHash": "015ea6874d4e1d4760d5871fb64d6947ac7f1ed7", "lineupsHash": "6d64f65880b5e4735caa77b08a78219530d05d77", "statsHash": "16a34a180ef76f3374a501caa2b7d2fee6114b64", "referees": [ { "type": "referee", "idReferee": 11861, "refereeName": "D. Orsato" }, { "type": "assitant referee", "idReferee": 15953, "refereeName": "V. Vecchi" }, { "type": "assitant referee", "idReferee": 12506, "refereeName": "D. Margani" }, { "type": "assitant referee", "idReferee": 131408, "refereeName": "A. Santoro" } ] }, { "id": 217132, "idCountry": 45, "countryName": "Italy", "idLeague": 318, "leagueName": "Serie A", "idSeason": 4270, "seasonName": "Serie A - 2021/2022", "idHome": 862, "homeName": "Bologna", "idAway": 865, "awayName": "Atalanta", "idStage": 4184, "idVenue": null, "venueName": null, "date": "2022-03-20 19:45:00", "status": "finished", "round": 30, "attendance": null, "team_home_90min_goals": 0, "team_away_90min_goals": 1, "team_home_ET_goals": 0, "team_away_ET_goals": 0, "team_home_PEN_goals": 0, "team_away_PEN_goals": 0, "team_home_1stHalf_goals": 0, "team_away_1stHalf_goals": 0, "team_home_2ndHalf_goals": 0, "team_away_2ndHalf_goals": 1, "elapsed": 0, "elapsedPlus": 0, "eventsHash": "da74a69c70cae017c350f1a4c427b46cd622a3a8", "lineupsHash": "cdc8cd82b532560f0e7e8b2b0b2032541241fd97", "statsHash": "dcb78af26f996bb0aa5209d4492ef9d7ff047966", "referees": null } ], "pagination": { "page": 1, "itemsPerPage": 25, "hasNextPage": false, "hasPrevPage": false } } }
 	match = resp.data.data[0];
+	img = await jimp.read('vs.jpg');
+	img_home = await jimp.read("https://cdn.elenasport.io/badges/150x150/"+match.idHome);
+	img_away = await jimp.read("https://cdn.elenasport.io/badges/150x150/"+match.idAway);
+	img.composite(img_home, 10, 105); //0, 360/2 - 150/2
+	img.composite(img_away, 480, 105);
+	img.write(match.idHome+"vs"+match.idAway+".jpg");
 	msg = {
 		embeds: [
 			{
@@ -45,11 +51,11 @@ try{
 				"url": "https://elenasport-io1.p.rapidapi.com",
 				"color": 9532993,
 				"timestamp": new Date(),
-				"image": {
+				/*"image": {
 					"url": "https://cdn.elenasport.io/badges/150x150/"+match.idHome,
 					"height": 150,
 					"width": 150
-				},
+				},*/
 				"fields": [
 					{
 						"name": "per scommettere usa (il messaggio non verrà visto dagli altri utenti):",
@@ -69,6 +75,9 @@ try{
 					"width": 50
 				},
 			}*/
+		],
+		files: [
+			match.idHome+"vs"+match.idAway+".jpg"
 		]
 	}
 	console.log(msg);
