a697d4b2ff0aca84034219b64501781ccd17c023
koraynilay <koray.fra@gmail.com>
2022-09-04 17:09:09 +0200

    Clearer description of options for /bet

--- commands/bet.js
+++ commands/bet.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const currency_name = "<:shadowsdesign:1005783063390859294>";
 
 exports.data = new SlashCommandBuilder()
 	.setName('bet')
@@ -8,18 +9,20 @@ exports.data = new SlashCommandBuilder()
 			.setName('value')
 			.setDescription('Usa 1, X, 2, 1X, X2 o 12')
 			.setRequired(true)
-			.addChoice('1',  '1')
-			.addChoice('X',  'X')
-			.addChoice('2',  '2')
-			.addChoice('1X', '1X')
-			.addChoice('X2', 'X2')
-			.addChoice('12', '12'))
-	.addIntegerOption(
+			.addChoices(
+				{ name: '1 (vince la squadra in casa)',  	value: '1' },
+				{ name: 'X (finisce in pareggio)',  		value: 'X' },
+				{ name: '2 (vince la squadra in trasferta)',  	value: '2' },
+				{ name: '1X (squadra in casa o pareggio)', 	value: '1X' },
+				{ name: 'X2 (squadra in trasferta o pareggio)',	value: 'X2' },
+				{ name: '12 (squadra in casa o in trasferta)', 	value: '12' }
+			)
+	).addIntegerOption(
 		option => option
 			.setName('bet')
-			.setDescription('Quanto stai scommettendo')
-			.setRequired(true))
-	.addStringOption(
+			.setDescription('Quanti shadows stai scommettendo')
+			.setRequired(true)
+	).addStringOption(
 		option => option
 			.setName('id')
 			.setDescription('`id` della partita (vedi il messaggio della partita)')
